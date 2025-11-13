3ed250b1defa153c6aadd2efcad2194177263404
goden <goden@Krishnasaigodena>
2025-11-13 08:41:18 +0530

    Added print statement How are you

--- src/test/java/campaigntest/CreateCampaignTest.java
+++ src/test/java/campaigntest/CreateCampaignTest.java
@@ -56,6 +56,7 @@ public class CreateCampaignTest extends BaseClass {
 				String msg = toastMsg.getText();
 				homePage.getCloseToastMsgBtn().click();
 				assertTrue(msg.contains("Successfully Added"));
+				System.out.println("How are you");
 }  
 	
 	@Test(groups = "regression")
