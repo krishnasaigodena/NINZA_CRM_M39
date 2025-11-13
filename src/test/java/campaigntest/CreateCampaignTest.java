927c50bb5ecdc492a30ef080159b41f1685a37a2
goden <goden@Krishnasaigodena>
2025-11-13 08:35:22 +0530

    added print stmnt Hi

--- src/test/java/campaigntest/CreateCampaignTest.java
+++ src/test/java/campaigntest/CreateCampaignTest.java
@@ -56,6 +56,7 @@ public class CreateCampaignTest extends BaseClass {
 				String msg = toastMsg.getText();
 				homePage.getCloseToastMsgBtn().click();
 				assertTrue(msg.contains("Successfully Added"));
+				System.out.println("Hi");
 }  
 	
 	@Test(groups = "regression")
